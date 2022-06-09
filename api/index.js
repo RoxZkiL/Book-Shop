84d45429918241581c9d33d33d7607806b4af994
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-06-09 17:53:04 -0300

    -

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.js"
+
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
