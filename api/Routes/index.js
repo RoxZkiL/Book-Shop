eb5e0f09aebc4694f0bd0c91746e3c6fc401c1fd
RoxZkiL <mttrox@gmail.com>
2022-06-10 17:39:02 -0400

    puto franx10

--- api/Routes/index.js
+++ api/Routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
-const topCinco = require("./Top5");
-const topDieZ = require("./Top10");
-const detail = require("./Details");
-const userDetail = require("./DetailUser")
+const top5 = require("./Top5");
+const top10 = require("./Top10");
+const bookdetail = require("./DetailBook");
+const userDetail = require("./DetailUser");
 
 const router = Router();
 
-router.use("/topCinco", topCinco);
-router.use("/topDieZ", topDieZ);
+router.use("/topCinco", top5);
+router.use("/topDieZ", top10);
 router.use("/detail", detail);
 router.use("/search", userDetail);
 
