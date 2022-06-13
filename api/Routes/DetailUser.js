c8ff69ff5e75ebc1e5b29186223f02fa050d8f9d
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-06-13 15:19:05 -0300

    its all done

--- api/Routes/DetailUser.js
+++ api/Routes/DetailUser.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const bd_users = require("../Database/usuarios.json");
+const bd_regalos = require("../Database/regalos.json");
+const { idGroups } = require("./utils/utils");
 
 router.get("/:id", (req, res) => {
   const idUser = req.params.id;
   const user = bd_users.find((e) => e.id === Number(idUser));
+
+  const allGroupsContact = idGroups(bd_regalos);
+  if (user) {
+    for (let i = 0; i < allGroupsContact.length; i++) {
+      if (allGroupsContact[i].includes(Number(idUser))) {
+        user.group = allGroupsContact[i].slice(0, 5);
+        if (allGroupsContact[i].length > 5) {
+          user.more = true;
+          break;
+        }
+      }else {
+        user.more = false;
+      }
+    }
+  }
+
   if (user) {
     res.status(200).send(user);
   } else {
